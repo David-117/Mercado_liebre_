4b2d79ce6a822c05c75af3ff8c1b87e55b84ab66
David Gomez <david.gomez.fca@gmail.com>
2022-06-10 19:38:57 -0500

    Proyecto Mercado Liebre

---
+++ README.md
@@ -0,0 +1,25 @@
+> # Mercado Liebre
+
+Este proyecto es una simulacion del sitio web Mercado libre, en cual la finalidad es poner en practica las diferentes tecnologias tanto del front end como del back end.
+
+> - 		 Autor:    David Gomez  
+
+------------
+
+
+
+
+
+> ## Tecnologias implementadas 
+
+> -  Javascript
+- Express
+- Node js
+- HTML
+- CSS
+
+------------
+
+
+** Para el uso del mismo, se debe tener instalado express en su repositorio local donde vaya clonar el repo. Ya que el proyecto tiene instanciado variables haciendo uso de express **
+# Mercado Liebre_
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const res = require('express/lib/response');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+
+app.get('/registro', (request, response) => {
+    response.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+
+app.get('/login', (request, response) => {
+    response.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+
+
+/* ------Servidor en escucha ---*/
+app.listen(3000, () => {
+    console.log('Servidor corriendo');
+});
